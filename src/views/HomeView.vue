c37b929a73c7320ed63a7c05bf594b3f3f34561e
songchengdong35 <576596778@qq.com>
2025-05-14 14:15:34 +0800

    feat: 适配移动端

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,5 +4,15 @@
 <template>
   <div class="home-content">
     <van-button type="primary">button</van-button>
+    <div class="home-body"></div>
   </div>
 </template>
+<style lang="less" scoped>
+.home-content {
+  .home-body {
+    width: 100px;
+    height: 100px;
+    background-color: red;
+  }
+}
+</style>
